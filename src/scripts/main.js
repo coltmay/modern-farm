795415e0a0da6202326074e3825e4c5a9038f87b
ColtMay <colten_mayberry@pm.me>
2021-02-27 17:13:50 -0600

    added seed producing modules

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,8 @@
 import { createPlan } from "./plan.js"
+import { createAsparagus } from "./seeds/asparagus.js"
+import { createCorn } from "./seeds/corn.js"
+import { createPotato } from "./seeds/potato.js"
+import { createSoybean } from "./seeds/soybean.js"
+import { createSunflower } from "./seeds/sunflower.js"
+import { createWheat } from "./seeds/wheat.js"
 const yearlyPlan = createPlan()
-
-console.log(yearlyPlan);
